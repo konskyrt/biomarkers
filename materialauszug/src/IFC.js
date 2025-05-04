02afd90bf112ec1a4b0d5c452f5e51a1079067ed
Konstantinos <kkyrtsonis@ambergloglay.com>
2025-05-04 16:06:47 +0200

    Keep only backend/ and frontend/ on old-version

--- materialauszug/src/IFC.js
+++
@@ -1,8 +0,0 @@
-import { IfcImporter } from "@thatopen/fragments";
-
-const serializer = new IfcImporter();
-serializer.wasm = { absolute: true, path: "https://unpkg.com/web-ifc@0.0.68/" };
-
-export {
-    serializer
-}
--- materialauszug/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-  <header className="bg-white w-full h-[40px] shadow" />
-);
-
-export default Header;
